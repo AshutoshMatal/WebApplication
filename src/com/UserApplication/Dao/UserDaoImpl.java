9d2ffe58c859ccae8915d00f13ba89cd136c403c
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-02 00:59:15 +0530

    MODIFIED:Get user method in User dao Implementation and Intendation and comments are used properly

--- src/com/UserApplication/Dao/UserDaoImpl.java
+++ src/com/UserApplication/Dao/UserDaoImpl.java
@@ -1,5 +1,8 @@
+package com.UserApplication.Dao;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import com.UserApplication.Model.User;
 import com.UserApplication.utility.DB_Utility;
@@ -7,12 +10,15 @@ import com.UserApplication.utility.DB_Utility;
 public class UserDaoImpl implements UserDao {
 	static Connection con;
 	static PreparedStatement ps;
+//INSERT USER IS IMPLEMENTED 
 	@Override
 	public int insertUser(User u) {
 		int status=0;
 		try
 		{
+			//CONNECTION IS DONE HERE
 			con=DB_Utility.getCon();
+			//INSERTING A QUERY
 			ps=con.prepareStatement("insert into login values(?,?)");
 			ps.setString(1,u.getUsername());	
 			ps.setString(2,u.getPassword());
@@ -26,4 +32,35 @@ public class UserDaoImpl implements UserDao {
 		}
 		return status;
 	}
+//GET USER  IS IMPLEMENTED
+	@Override
+	public User getUser(String username, String pass)
+	{ 
+		// USER CLASS 
+		User c=new User();
+		try
+		{
+			//CONNECTION IS CREATED
+			con=DB_Utility.getCon();
+			ps=con.prepareStatement("Select * from login where username=? and password=?");
+			ps.setString(1,username);
+			ps.setString(2,pass);
+
+			ResultSet rs=ps.executeQuery();
+			while(rs.next())
+			{
+				c.setUsername(rs.getString(1));
+				c.setPassword(rs.getString(2));
+				c.setName(rs.getString(3));
+
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		// TODO Auto-generated method stub
+		return c;
+	}
+
 }
