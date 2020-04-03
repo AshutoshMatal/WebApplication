8475bfbad0b94ae25a1e5dfae19a99455951c8e7
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-03 21:11:02 +0530

    ADDED:For the Databaseconnectivity local host is added with url

---
+++ src/com/UserApplication/utility/utility.java
@@ -0,0 +1,8 @@
+package com.UserApplication.utility;
+
+public interface utility {
+	public static String userName="root";
+	public static String password="abc";
+	public static String connUrl= "jdbc:mysql://localhost:3306/bridgelabz?useSSL=false";
+
+}
